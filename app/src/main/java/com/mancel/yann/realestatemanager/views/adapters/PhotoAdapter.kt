3e33794b519b862a502abe13c517181d18407e50
Yann Mancel <yann.mancel@hotmail.fr>
2020-02-21 22:22:01 +0100

    Add PhotoAdapter for DetailsFragment

---
+++ app/src/main/java/com/mancel/yann/realestatemanager/views/adapters/PhotoAdapter.kt
@@ -0,0 +1,78 @@
+package com.mancel.yann.realestatemanager.views.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.mancel.yann.realestatemanager.R
+import com.mancel.yann.realestatemanager.utils.PhotoDiffCallback
+import kotlinx.android.synthetic.main.item_photo_real_estate.view.*
+
+/**
+ * Created by Yann MANCEL on 21/02/2020.
+ * Name of the project: RealEstateManager
+ * Name of the package: com.mancel.yann.realestatemanager.views.adpaters
+ *
+ * A [RecyclerView.Adapter] subclass.
+ */
+class PhotoAdapter : RecyclerView.Adapter<PhotoAdapter.PhotoViewHolder>() {
+
+    // NESTED CLASSES ------------------------------------------------------------------------------
+
+    /**
+     * A [RecyclerView.ViewHolder] subclass.
+     */
+    class PhotoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    // FIELDS --------------------------------------------------------------------------------------
+
+    private var mPhotos: List<String> = mutableListOf()
+
+    // METHODS -------------------------------------------------------------------------------------
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        // Creates the View thanks to the inflater
+        val view = LayoutInflater.from(parent.context)
+                                 .inflate(R.layout.item_photo_real_estate, parent, false)
+
+        return PhotoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        // Data
+        val data = this.mPhotos[position]
+
+        // Image
+        Glide.with(holder.itemView)
+             .load(R.drawable.ic_home)
+             .centerCrop()
+             .fallback(R.drawable.ic_add)
+             .error(R.drawable.ic_edit)
+             .into(holder.itemView.item_photo_image)
+
+        // Description
+        holder.itemView.item_photo_description.text = data
+    }
+
+    override fun getItemCount(): Int = this.mPhotos.size
+
+    // -- Photo --
+
+    /**
+     * Updates data of [PhotoAdapter]
+     * @param newPhotos a [List] of [String]
+     */
+    fun updateData(newPhotos: List<String>) {
+        // Optimizes the performances of RecyclerView
+        val diffCallback  = PhotoDiffCallback(this.mPhotos, newPhotos)
+        val diffResult  = DiffUtil.calculateDiff(diffCallback )
+
+        // New data
+        this.mPhotos = newPhotos
+
+        // Notifies adapter
+        diffResult.dispatchUpdatesTo(this)
+    }
+}
--- app/src/main/java/com/mancel/yann/realestatemanager/views/fragments/DetailsFragment.kt
+++ app/src/main/java/com/mancel/yann/realestatemanager/views/fragments/DetailsFragment.kt
@@ -1,7 +1,12 @@
 package com.mancel.yann.realestatemanager.views.fragments
 
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mancel.yann.realestatemanager.R
+import com.mancel.yann.realestatemanager.views.adapters.PhotoAdapter
 import com.mancel.yann.realestatemanager.views.bases.BaseFragment
+import kotlinx.android.synthetic.main.fragment_details.view.*
 
 /**
  * Created by Yann MANCEL on 20/02/2020.
@@ -12,11 +17,50 @@ import com.mancel.yann.realestatemanager.views.bases.BaseFragment
  */
 class DetailsFragment : BaseFragment() {
 
+    // FIELDS --------------------------------------------------------------------------------------
+
+    private lateinit var mAdapter: PhotoAdapter
+
     // METHODS -------------------------------------------------------------------------------------
 
     // -- BaseFragment --
 
     override fun getFragmentLayout(): Int = R.layout.fragment_details
 
-    override fun configureDesign() {}
+    override fun configureDesign() {
+        // UI
+        this.configureRecyclerView()
+
+        // Test
+        this.mAdapter.apply {
+            updateData(listOf("Room", "Bedroom", "Bathroom", "Bed"))
+        }
+    }
+
+    // -- RecyclerView --
+
+    /**
+     * Configures the [RecyclerView]
+     */
+    private fun configureRecyclerView() {
+        // Adapter
+        this.mAdapter = PhotoAdapter()
+
+        // LayoutManager
+        val viewManager = LinearLayoutManager(this.context,
+                                              LinearLayoutManager.HORIZONTAL,
+                                             false)
+
+        // Divider
+        val divider = DividerItemDecoration(this.context,
+                                            DividerItemDecoration.HORIZONTAL)
+
+        // RecyclerView
+        this.mRootView.fragment_details_RecyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            addItemDecoration(divider)
+            adapter = mAdapter
+        }
+    }
 }
